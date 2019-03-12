be1ea155cd828bbf4e1a2e8b98f8a73e9da35e7a
parkertimmins <parkertimmins@gmail.com>
2019-03-12 00:31:40 -0500

    add separate draw completely button

--- simple_generator.js
+++ simple_generator.js
@@ -5,6 +5,7 @@ function rotations_to_complete(circles) {
 }
 
 
+const to_radians = degree => degree * Math.PI / 180;
 
 function add_altitude(lat_long_point, altitude) {
 	return {
@@ -18,7 +19,6 @@ function csv_template(waypoints) {
 	const format_line = waypoint => `${waypoint.latitude},${waypoint.longitude},${waypoint.altitude}`
 	const waypoints_str = waypoints.map(format_line).join("\n")
 	return "latitude,longitude,height\n" + waypoints_str;
-
 }
 
 function kml_template(waypoints) {
@@ -88,7 +88,6 @@ function split_circle_line(radius_type) {
     }
 }
 
-
 function draw_line(angle, offset, length, canvas_id) {
     var canvas = document.getElementById(canvas_id);
     
@@ -116,7 +115,7 @@ function clear_canvas(canvas_id) {
 function convert_x_y_to_canvas(x, y, canvas) {
 	var right = (canvas.width / 2) + x
 	var down = (canvas.height / 2) - y
-	return {right: right, down: down}
+	return { right, down }
 }
 
 function draw_2_point_line(start_point, end_point, color, canvas_id) {	
@@ -202,7 +201,7 @@ function get_pen_from_last_circle(circle_offset, circle_angle, pen_radius) {
 
 
 // num loop inside parent in single first child loop
-function get_revolutions(rotations) {
+function get_revolutions_amount(rotations) {
     var revolutions = [0, 1]
     for (var c = 2; c < rotations.length; c++) {
         revolutions[c] = (rotations[c-1] - rotations[c-2]) + revolutions[c-1]
@@ -211,7 +210,7 @@ function get_revolutions(rotations) {
 }
 
 // num 2pi rotations of circle in single first child loop
-function get_rotations(circles) {
+function get_rotations_amount(circles) {
     var rotations = [0]
     for (var c = 1; c < circles.length; c++) {
         const parent = circles[c-1]
@@ -229,7 +228,7 @@ const CCW = 1 // counter clockwise
 const same = x => x
 const opposite = x => -x
 function get_rotate_directions(circles) {
-    const directions = [null]; 
+    const directions = [0]; 
     directions[1] = circles[1].inner ? CW : CCW
     for (var c = 2; c < circles.length; c++) {
         directions[c] = circles[c].inner ? same(directions[c-1]) : opposite(directions[c-1])
@@ -238,15 +237,113 @@ function get_rotate_directions(circles) {
 }
 
 function get_revolve_directions(circles, rotate_directions) {
-    const directions = [null]; 
+    const directions = [0]; 
     for (var c = 1; c < circles.length; c++) {
         directions[c] = circles[c].inner ? opposite(rotate_directions[c]) : same(rotate_directions[c])
     }
     return directions 
 }
 
+function get_single_point_settings(angle_biggest_rotor, circles, pen_radius, rotations, revolutions) {
+
+    const circle_settings = [];
+    let offset = {x: 0, y: 0};
+
+    for (let i = 1; i < circles.length; i++) {
+        const parent = circles[i-1];
+        const circle = circles[i];
+       
+        const angle_around = revolutions[i] * angle_biggest_rotor;
+        const center_radius = circle.inner ? parent.radius - circle.radius : parent.radius + circle.radius;
+        const center = get_point(center_radius, angle_around, offset);
+        const angle_rotated = rotations[i] * angle_biggest_rotor;
+        circle_settings.push({ center, angle_rotated });
+
+        offset = center;
+    } 
+        
+    const last_circle_angle_rotated = circle_settings[circle_settings.length-1].angle_rotated
+    const spiro_point = get_point(pen_radius, last_circle_angle_rotated, offset);
+
+    return { spiro_point, circle_settings };
+}
 
 
+function get_sample_points(inputs, input_rotations) {
+    
+    const angle_diff_degrees = inputs.end_segment_degrees - inputs.start_segment_degrees;
+    const angle_diff_radians = to_radians(angle_diff_degrees);
+    const start_angle_radians = to_radians(inputs.start_segment_degrees);
+
+    const angle_increment = angle_diff_radians / (inputs.num_sample_points - 1);
+    
+    const point_samples = [];
+    for (let i = 0; i < inputs.num_sample_points; i++) {
+        const angle_biggest_rotor = i * angle_increment + start_angle_radians;
+        const sample = get_single_point_settings(angle_biggest_rotor, inputs.circles, inputs.pen_radius, input_rotations.rotations, input_rotations.revolutions);
+        point_samples.push(sample);
+    }
+
+    return point_samples;
+}
+
+function get_input_rotations(circles) {
+    const rotate_amounts = get_rotations_amount(circles);
+    const rotate_directions = get_rotate_directions(circles);
+    const rotations = circles.map((e, i) => rotate_amounts[i] * rotate_directions[i])
+    
+    const revolve_amounts = get_revolutions_amount(rotate_amounts);
+    const revolve_directions = get_revolve_directions(circles, rotate_directions);
+    const revolutions = circles.map((e, i) => revolve_amounts[i] * revolve_directions[i])
+    
+    return { rotations, revolutions };
+}
+
+function get_draw_inputs() {
+    const stator_radius = parseInt(document.getElementById("stator-radius").value);
+    const rotor_lines = document.getElementById("radius-input").value
+        .split("\n").filter(line => line.length > 0);
+   
+    let circles = [ {radius: stator_radius} ];
+    circles = circles.concat(rotor_lines.map(split_circle_line));
+
+    const pen_radius = parseInt(document.getElementById("pen-radius").value)
+
+    const num_sample_points = parseInt(document.getElementById("sample-points").value);
+    const start_segment_degrees = parseFloat(document.getElementById("start-segment").value);
+    const end_segment_degrees = parseFloat(document.getElementById("end-segment").value);
+
+    return {
+        circles,
+        pen_radius,
+        num_sample_points,
+        start_segment_degrees,
+        end_segment_degrees
+    } 
+}
+
+function validate_lat_long(input) {
+    const error_msg = "Latitude, Longitude input must be two comma separated value decimal numbers between -180 and 180"
+
+    if (input === null) {
+        return { error: error_msg };
+    }
+
+    const parts = input.split(/,| /)
+    if (parts.length !== 2) {
+        return { error: error_msg };
+    }
+
+    const lat = parseFloat(parts[0]);
+    const long = parseFloat(parts[1]);
+   
+    // NaN will fail 
+    if (lat < -180 || lat > 180 || long < -180 || long > 180) {
+        return { error: error_msg };
+    }
+
+    return { value: { latitude: lat, longitude: long } };
+}
 
 
 ////////////////////////////// run ////////////////////////////////////
@@ -257,36 +354,23 @@ function get_revolve_directions(circles, rotate_directions) {
 ////// global values for step function /////
 
 
-// required inputs to draw
-// circles - stator, rotors
-// pen_radius
-// sample_points
-// start-segment
-// end-segment
-
 let inputs = null; 
 let input_rotations = null; 
 let point_samples = null; 
 let current_point = 0;
 
 
-
-let theta_increment = 0
-
-let points = []
-
-
-// big circle angle radians
-let t_biggest = 0 
-
-// TODO change to be time based
 function step() {
   
     const point_sample = point_samples[current_point];
   
-    // draw all circles and the spiro point
+    // draw all circles 
     draw_state_at_point(inputs.circles, inputs.pen_radius, point_sample);
 
+    // draw point 
+    const spiro = point_sample.spiro_point;
+    draw_point(spiro.x, spiro.y, "#0000ff", "canvas-static")
+    
     // draw line between last
     if (current_point >= 1) {
         const last_point_sample = point_samples[current_point-1];
@@ -350,12 +434,28 @@ function draw_state_at_point(circles, pen_radius, point_sample) {
     // draw pen
     const last_circle_settings = point_sample.circle_settings[point_sample.circle_settings.length - 1];
     draw_line(last_circle_settings.angle_rotated, last_circle_settings.center, pen_radius, "canvas-refreshing")
-      
-    // draw point 
-    const spiro = point_sample.spiro_point;
-    draw_point(spiro.x, spiro.y, "#0000ff", "canvas-static")
 }
 
+function drawCompletely() {
+    clear_canvas("canvas-refreshing")
+    clear_canvas("canvas-static")
+
+    inputs = get_draw_inputs();
+    input_rotations = get_input_rotations(inputs.circles);
+    point_samples = get_sample_points(inputs, input_rotations);
+
+    point_samples.forEach((point_sample, i) => {
+        // draw point 
+        const spiro = point_sample.spiro_point;
+        draw_point(spiro.x, spiro.y, "#0000ff", "canvas-static")
+        
+        // draw line between last
+        if (i >= 1) {
+            const last_point_sample = point_samples[i-1];
+            draw_2_point_line(point_sample.spiro_point, last_point_sample.spiro_point, "#ff2626", "canvas-static")
+        } 
+    });
+}
 
 
 function startAnimation() {
@@ -389,128 +489,6 @@ function download(filename, text) {
 // on load code  ////////////////////////////////////////////////
 window.onload = function () {
        document.getElementById("start-animation").onclick=startAnimation;
+       document.getElementById("draw-completely").onclick=drawCompletely;
 }
 
-
-////// Validation functions ///////////////////
-
-function get_single_point_settings(angle_biggest_rotor, inputs, input_rotations) {
-    const circles = inputs.circles;
-    const pen_radius = inputs.pen_radius;
-    const rotations = input_rotations.rotations;
-    const revolutions = input_rotations.revolutions;
-    const rotate_directions = input_rotations.rotate_directions;
-    const revolve_directions = input_rotations.revolve_directions;
-
-
-    const circle_settings = [];
-    let offset = {x: 0, y: 0};
-
-    for (let i = 1; i < circles.length; i++) {
-        const parent = circles[i-1];
-        const circle = circles[i];
-       
-        const angle_around = revolve_directions[i] * revolutions[i] * angle_biggest_rotor;
-        const center_radius = circle.inner ? parent.radius - circle.radius : parent.radius + circle.radius;
-        const center = get_point(center_radius, angle_around, offset);
-        const angle_rotated = rotate_directions[i] * rotations[i] * angle_biggest_rotor;
-        
-        circle_settings.push({
-            center: center,
-            angle_rotated: angle_rotated   
-        });   
-
-        offset = center;
-    } 
-        
-    const last_circle_angle_rotated = circle_settings[circle_settings.length-1].angle_rotated
-    const spiro_point = get_point(pen_radius, last_circle_angle_rotated, offset);
-    return {
-        spiro_point: spiro_point,
-        circle_settings: circle_settings
-    };      
-}
-
-
-function get_sample_points(inputs, input_rotations) {
-    
-    const angle_diff_degrees = inputs.end_segment_degrees - inputs.start_segment_degrees;
-    const angle_diff_radians = to_radians(angle_diff_degrees);
-    const start_angle_radians = to_radians(inputs.start_segment_degrees);
-
-    const angle_increment = angle_diff_radians / (inputs.num_sample_points - 1);
-    
-    const point_samples = [];
-    
-    for (let i = 0; i < inputs.num_sample_points; i++) {
-        const angle_biggest_rotor = i * angle_increment;
-        const sample = get_single_point_settings(angle_biggest_rotor, inputs, input_rotations);
-        point_samples.push(sample);
-    }
-
-    return point_samples;
-}
-
-function to_radians(degree) {
-    return degree * Math.PI / 180;
-}
-
-
-function get_input_rotations(circles) {
-    const rotations = get_rotations(circles);
-    const revolutions = get_revolutions(rotations);
-    const rotate_directions = get_rotate_directions(circles);
-    const revolve_directions = get_revolve_directions(circles, rotate_directions);
-    return { 
-        rotations,
-        revolutions,
-        rotate_directions,
-        revolve_directions
-    };
-}
-
-function get_draw_inputs() {
-    const stator_radius = parseInt(document.getElementById("stator-radius").value);
-    const rotor_lines = document.getElementById("radius-input").value
-        .split("\n").filter(line => line.length > 0);
-   
-    let circles = [ {radius: stator_radius} ];
-    circles = circles.concat(rotor_lines.map(split_circle_line));
-
-    const pen_radius = parseInt(document.getElementById("pen-radius").value)
-
-    const num_sample_points = parseInt(document.getElementById("sample-points").value);
-    const start_segment_degrees = parseFloat(document.getElementById("start-segment").value);
-    const end_segment_degrees = parseFloat(document.getElementById("end-segment").value);
-
-    return {
-        circles,
-        pen_radius,
-        num_sample_points,
-        start_segment_degrees,
-        end_segment_degrees
-    } 
-}
-
-function validate_lat_long(input) {
-    const error_msg = "Latitude, Longitude input must be two comma separated value decimal numbers between -180 and 180"
-
-    if (input === null) {
-        return { error: error_msg };
-    }
-
-    const parts = input.split(/,| /)
-    if (parts.length !== 2) {
-        return { error: error_msg };
-    }
-
-    const lat = parseFloat(parts[0]);
-    const long = parseFloat(parts[1]);
-   
-    // NaN will fail 
-    if (lat < -180 || lat > 180 || long < -180 || long > 180) {
-        return { error: error_msg };
-    }
-
-    return { value: { latitude: lat, longitude: long } };
-}
